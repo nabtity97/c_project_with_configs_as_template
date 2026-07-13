8e667ca68754dd0d4751be6339a0543572a428f3
Hesham Elnabtity <hesham.elnabtity@student.tugraz.at>
2026-07-13 23:46:44 +0200

    launch.json and tasks.json work fine, I managed to build and debug on powershell

---
+++ main.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+    
+    printf("You entered: \n");
+    int my_age;
+    scanf("%d", &my_age);
+    printf("your age is %d", my_age);
+    return 0;
+}
